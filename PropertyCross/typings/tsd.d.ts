7ffd75154434eaf0d3e512561192a444b333b3a4
Nils Meissner <nmeissner@scottlogic.co.uk>
2016-03-30 13:41:06 +0100

    Delete unnecessary example app

--- PropertyCross/typings/tsd.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="es6-promise/es6-promise.d.ts" />
-/// <reference path="rx/rx-lite.d.ts" />
-/// <reference path="rx/rx.d.ts" />
-/// <reference path="../node_modules/reflect-metadata/reflect-metadata.d.ts" />
